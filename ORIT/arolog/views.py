18c572cf5e987618b72320b6c4afa51f2015b782
vgrid <im@vgrid.ru>
2022-09-15 01:58:03 +0300

    В контексте главной страницы теперь отображаются только записи текущего пользователя

--- ORIT/arolog/views.py
+++ ORIT/arolog/views.py
@@ -1,24 +1,39 @@
-from django.shortcuts import render
+from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views import generic
 from .models import AROLogModel
+from mo.models import *
 
 
 @method_decorator(login_required, name='dispatch')
 class Home(generic.ListView):
     template_name = 'arolog/home.html'
-    context_object_name = 'latest_rec_list'
+    # context_object_name = 'latest_rec_list'
+
     # paginate_by = 25
 
     def get_queryset(self):
-        """ возвращает последние 10 записей """
-        return AROLogModel.objects.order_by('-edit_datetime')[:10]
+        """ возвращает последние 10 записей
 
+        return AROLogModel.objects.order_by('-edit_datetime')[:10]
+        """
+        q = Q(
+            registrator__user=self.request.user
+        )
+        return AROLogModel.objects.filter(q)
+
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super().get_context_data(*args, **kwargs)
+    #     context['mycontext'] = 'Мой контекст'
+    #     context['molist'] = MOModel.objects.all()
+    #
+    #     return context
 
 @method_decorator(login_required, name='dispatch')
 class AListView(generic.ListView):
     paginate_by = 25
+
     def get_queryset(self):
         return AROLogModel.objects.order_by('-edit_datetime')
 
@@ -38,6 +53,6 @@ class SearchResultsView(generic.ListView):
     def get_queryset(self):
         query = self.request.GET.get('q')
         object_list = AROLogModel.objects.filter(
-            mh_num__icontains=query # поиск по номеру истории болезни
+            mh_num__icontains=query  # поиск по номеру истории болезни
         ).distinct()
         return object_list
